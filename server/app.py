6d6992b25e27ac82a1e6ff679d9a69205f304d0c
Eric Maranga <ekirira22@gmail.com>
2024-04-04 20:41:19 +0300

    queried a db in flask app

--- server/app.py
+++ server/app.py
@@ -1,9 +1,13 @@
 # server/app.py
 
-from flask import Flask
+from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Pet
+
+'''
+    DATABASE CONFIGURATION
+'''
 
 # create a Flask application instance 
 app = Flask(__name__)
@@ -19,6 +23,45 @@ migrate = Migrate(app, db)
 
 # initialize the Flask application to use the database
 db.init_app(app)
+
+'''
+    SERVING DATABASE RECORDS IN FLASK APP
+'''
+
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+    if pet:
+        response_body = f'<p>{pet.name} {pet.species}'
+        response_status = 200
+    else:
+        response_body = f'<p>Pet {id} not found'
+        response_status = 404
+
+    response = make_response(
+        response_body,
+        response_status,
+    )
+    return response
+
+@app.route('/pets/<string:species>')
+def pet_by_species(species):
+    pets = Pet.query.filter_by(species = species).all()
+    response_body = f'<p>There are {len(pets)} {species}\'s</p>'
+
+    if pets:
+        for pet in pets:
+            response_body += f'<p>{pet.name} | {pet.species}</p>'
+        response_status = 200
+    else:
+        response_body = f'<p>Pet {species} not found'
+        response_status = 404
+
+    response = make_response(
+        response_body,
+        response_status,
+    )
+    return response
 
 
 if __name__ == '__main__':
